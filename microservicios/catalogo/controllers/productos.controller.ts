baaffe08049f34bb9f48afc32ab0760115b54a16
andressamuel13 <samuelma236@gmail.com>
2025-11-23 20:52:15 -0500

    controller de producto casi terminado

---
+++ microservicios/catalogo/controllers/productos.controller.ts
@@ -0,0 +1,71 @@
+import { Request,Response } from "express";
+import { ProductosRepository } from "../repository/productos.repository.js";
+import type { ProductosDto } from "../DTO/productosDto.js";
+
+export class ProductoController{
+    static async createProduct(req:Request,res:Response){
+        try{
+            const {id,nombre, descripcion, categoria, imagen,created_at, updated_at} = req.body
+            const newProducto:ProductosDto = {
+                id,
+                nombre,
+                descripcion, 
+                categoria, 
+                imagen,
+                created_at, 
+                updated_at
+            }
+            if(!id || !nombre || !descripcion || !categoria || !imagen || !created_at || !updated_at){
+                res.status(400).json({message:"Faltan datos obligatorios"})
+                return
+            }
+            const result = await ProductosRepository.crearProducto(newProducto)
+            res.status(201).json({message:"Producto creado", id: result})
+        }catch(error){
+            res.status(500).json({message:"Error al crear el producto", error})
+            console.error("Error al crear el producto:", error)
+        }
+    }
+
+    static async getProducts(req:Request,res:Response){
+        try{
+            const producto = await ProductosRepository.obtenerProductos()
+            res.status(200).json({message:"Productos obtenidos correctamente", data:producto})
+        }catch(error){
+            res.status(500).json({message:"Error al obtener los productos", error})
+            console.error("Error al obtener los productos:", error)
+        }
+    }
+
+    static async getProductById(req:Request,res:Response){
+        try{
+            const {id} = req.params
+            const producto = await ProductosRepository.obtenerProductoPorId(Number(id))
+            res.status(200).json({message:"Producto obtenido correctamente", data:producto})
+            if(!producto){
+                res.status(404).json({message:"Producto no encontrado"})
+                return
+            }
+        }catch(error){
+            res.status(500).json({message:"Error al obtener el producto", error})
+            console.error("Error al obtener el producto:", error)
+        }
+    }
+
+    static async updateProductById(req:Request,res:Response){
+        try{
+            const{id} = req.params
+            const productoUpdates:Partial<ProductosDto> = req.body
+            const result = await ProductosRepository.actualizarProductoPorId(Number(id),productoUpdates)
+            res.status(200).json({message:"Producto actualizado correctamente", data:result})
+            return !result ? res.status(404).json({message:"Producto no encontrado"}) : null
+            // if(!result){
+            //     res.status(404).json({message:"Producto no encontrado"})
+            //     return
+            // }
+        }catch(error){
+            res.status(500).json({message:"Error al actualizar el producto", error})
+            console.error("Error al actualizar el producto:", error)
+        }
+    }
+}
