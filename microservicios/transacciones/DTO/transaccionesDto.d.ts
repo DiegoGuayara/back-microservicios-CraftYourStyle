8cc002b4cbf9c67d68643bdaf405e4b7514ce305
DiegoGuayara <diegofguayara9707@gmail.com>
2025-11-25 23:50:55 -0500

    Eliminé los archivos que no servían en las carpetas, lo que se compiló mal

--- microservicios/transacciones/DTO/transaccionesDto.d.ts
+++
@@ -1,7 +0,0 @@
-export interface TransaccionDto {
-    numero_de_cuenta: string;
-    tipo_de_cuenta: "debito" | "credito";
-    banco: string;
-    id_user: number;
-}
-//# sourceMappingURL=transaccionesDto.d.ts.map
--- microservicios/transacciones/controllers/transaccionesController.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Request, Response } from "express";
-export declare class TransaccionesController {
-    static crearTransaccion(req: Request, res: Response): Promise<void>;
-    static obtenerCuentas(req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
-    static updateUser(req: Request, res: Response): Promise<void>;
-    static eliminarCuenta(req: Request, res: Response): Promise<void>;
-}
-//# sourceMappingURL=transaccionesController.d.ts.map
